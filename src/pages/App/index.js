8265f83446c20d55b4dba74b13ee80bb30aaed4b
mnav0 <mnavracruz@gmail.com>
2020-04-01 13:07:52 -0400

    First commit

---
+++ src/pages/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './styles.css';
+// import component1 from '../component1';
+
+const App = () => {
+  return (
+    <div className={styles.app}>
+      <p>Sup bitch</p>
+    </div>
+  );
+};
+
+export default App;
