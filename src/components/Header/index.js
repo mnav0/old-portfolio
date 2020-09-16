cafb58752a4deefe27897c76d8a32a2a384ddddd
mnav0 <mnavracruz@gmail.com>
2020-09-16 14:07:58 -0400

    Add responsiveness for homepage

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -25,6 +25,9 @@ const Header = () => {
             <NavLink to="/about" onMouseEnter={() => updateHoverHead()}
                                 onMouseLeave={() => updateHoverHead()}
                                 className = {headerName}>magdalena navracruz</NavLink>
+            <NavLink to="/about" onMouseEnter={() => updateHoverHead()}
+                                onMouseLeave={() => updateHoverHead()}
+                                className = {headerName + " header-mobile"}>mn</NavLink>
         </div>
     );
 };
